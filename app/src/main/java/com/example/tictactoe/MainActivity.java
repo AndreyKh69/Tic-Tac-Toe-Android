186d5264d00623697ca27ddb2d2e5f3145b5e090
RomZil <zilber.rom@gmail.com>
2022-12-04 19:48:34 +0200

    rom - fix logic - 04/12/2022

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -92,24 +92,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (playerNum == 2) {
             imageView.setImageResource(R.drawable.owin);
         } else {
-//            Toast.makeText(this, "IT'S A DRAW", Toast.LENGTH_SHORT).show();
             imageView.setImageResource(R.drawable.nowin);
         }
-//        resetBoard();
     }
 
     // method to reset the board
     private void resetBoard() {
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 buttons[i][j].setTag("");
                 buttons[i][j].setImageResource(R.drawable.empty);
             }
         }
-        ImageView imageView = findViewById(R.id.imageviewStatus);
-        imageView.setImageResource(R.drawable.status);
-        roundCount = 0;
-        firstPlayer = true;
+        for (int i = 1; i < 9; i++) {
+            String winLine = "winLine" + i;
+            int resId = getResources().getIdentifier(winLine, "id", getPackageName());
+            ImageView imageViewLine = findViewById(resId);
+            imageViewLine.setVisibility(View.INVISIBLE);
+        }
+
+            ImageView imageView = findViewById(R.id.imageviewStatus);
+            imageView.setImageResource(R.drawable.status);
+            roundCount = 0;
+            firstPlayer = true;
+
     }
 
     private boolean checkFowWin() {
@@ -124,7 +131,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (field[i][0] == (field[i][1]) &&
                     field[i][0] == (field[i][2]) &&
                     field[i][0] != ("")) {
+
                 // set win line
+                if ( i == 0 ) {
+                    ImageView imageView = findViewById(R.id.winLine6);
+                    imageView.setVisibility(View.VISIBLE);
+                } else if ( i == 1 ) {
+                    ImageView imageView = findViewById(R.id.winLine7);
+                    imageView.setVisibility(View.VISIBLE);
+                } else {
+                    ImageView imageView = findViewById(R.id.winLine8);
+                    imageView.setVisibility(View.VISIBLE);
+                }
                 return true;
             }
         }
@@ -132,7 +150,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (field[0][i] == (field[1][i]) &&
                     field[0][i] == (field[2][i]) &&
                     field[0][i] != ("")) {
+
                 // set win line
+                if ( i == 0 ) {
+                    ImageView imageView = findViewById(R.id.winLine1);
+                    imageView.setVisibility(View.VISIBLE);
+                } else if ( i == 1 ) {
+                    ImageView imageView = findViewById(R.id.winLine2);
+                    imageView.setVisibility(View.VISIBLE);
+                } else {
+                    ImageView imageView = findViewById(R.id.winLine3);
+                    imageView.setVisibility(View.VISIBLE);
+                }
                 return true;
             }
         }
@@ -140,14 +169,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (field[0][0] == (field[1][1]) &&
                 field[0][0] == (field[2][2]) &&
                 field[0][0] != ("")) {
+
             // set win line
+            ImageView imageView = findViewById(R.id.winLine4);
+            imageView.setVisibility(View.VISIBLE);
             return true;
         }
 
         if (field[0][2] == (field[1][1]) &&
                 field[0][0] == (field[2][0]) &&
                 field[0][2] != ("")) {
+
             // set win line
+            ImageView imageView = findViewById(R.id.winLine5);
+            imageView.setVisibility(View.VISIBLE);
             return true;
         }
 
